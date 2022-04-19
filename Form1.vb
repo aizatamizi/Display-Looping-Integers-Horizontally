2aba5ead2c8909b6002ef7bb91227c82499aef71
ASUS <ASUS@LAPTOP-UDHPNQ86>
2022-04-20 01:26:34 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+        For n = 50 To 0 Step -5
+            TextBox1.Text = TextBox1.Text & n & vbCr + vbCrLf
+        Next n
+    End Sub
+End Class
